776beb5bc2fa45dcaa444ad50c0c68d7bc3346e9
Davin Tryon <davin@lockdown-2.local>
2023-10-06 12:56:40 +0100

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
