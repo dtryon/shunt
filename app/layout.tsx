7569ba44409431ccd9b0f9586121866c3c59b315
Davin Tryon <davin@lockdown-2.local>
2023-10-09 11:11:45 +0100

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex">
+          <img width="50px" src="/trusty-logo.svg" />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
