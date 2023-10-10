0b4309be03c3a23080d1fef7229043d94e2947ee
Davin Tryon <davin@lockdown-2.local>
2023-10-10 10:43:29 +0100

    no clue 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           </section>
         </header>
         <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
-          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-xl italic lg:flex">
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-xl lg:flex">
             {children}
           </div>
         </main>
---
+++ app/noclue1/page.tsx
@@ -0,0 +1,17 @@
+export default function Clue1() {
+  return (
+    <div>
+      <p className="text-center pb-24 italic">
+        Nice try! You've found a code scavenger hunt.
+      </p>
+      <a
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Get a prize?
+        </h5>
+      </a>
+    </div>
+  );
+}
