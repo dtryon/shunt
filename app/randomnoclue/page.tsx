56ceb9588a7630d0e5f6cdfbe8a71840b1b1022f
Amar Patel <amar.patel@rocketmail.com>
2023-10-10 19:25:36 +0100

    Make the clues driven by UUIDs as query parameter

--- app/randomnoclue/page.tsx
+++ app/randomnoclue/page.tsx
@@ -1,26 +1,37 @@
 import { promises as fs } from 'fs';
+import React from 'react';
 
-export default async function Page() {
+export interface Clue {
+    id: string;
+    message: string;
+    image: string;
+}
+
+export default async function Page({ searchParams }) {
     const file = await fs.readFile(process.cwd() + '/app/no-clue.json', 'utf8');
-    const data = JSON.parse(file);
+    const data: [Clue] = JSON.parse(file);
 
-    let x = Math.floor((Math.random() * 20) + 1);
-    const item = data[x];
-    console.log(item);
+    const clueId: String = searchParams.clue;
+    console.log(clueId);
+    let index = Math.floor((Math.random() * 20) + 1);
+    if (clueId) {
+        const match = data.findIndex((e) => e.id === clueId);
+        if (match >= 0) {
+            index = match;
+        }
+    }
+
+    const item = data[index];
 
     return (
         <div>
             <p className="text-center pb-24 italic">
                 {item.message}
             </p>
-            {/* <a
-        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Get a prize?
-        </h5>
-      </a> */}
+            {
+                item.image ?
+                    <img width="200px" src={item.image} /> : <div></div>
+            }
         </div>
     );
 }
