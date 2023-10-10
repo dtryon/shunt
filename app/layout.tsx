1eb5146d4bad29f257dfa4b288defb0cc17b7f20
Davin Tryon <davin@lockdown-2.local>
2023-10-10 08:33:33 +0100

    add some styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="flex">
-          <img width="50px" src="/trusty-logo.svg" />
+        <header className="flex items-center bg-sky-950">
+          <img className="flex-none" width="60px" src="/trusty-logo.svg" />
+          <section className="flex-auto">
+            <h2 className="pl-2 text-slate-300">October Scavenger Hunt</h2>
+          </section>
         </header>
-        {children}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-xl italic lg:flex">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
